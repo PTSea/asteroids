ac90657a97adb9a88bc27523059b6d68ce3d6950
PTSea <ptan09@gmail.com>
2025-06-30 20:23:16 -0700

    Forgot to push code between all these lessons, woops

--- main.py
+++ main.py
@@ -2,20 +2,44 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+import constants
+import sys
+from player import *
+from circleshape import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    my_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000)
+        for u in updatable:
+            u.update(dt)
+        for a in asteroids:
+            if a.isColliding(my_player):
+                print("GAME OVER!")
+                sys.exit()
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
